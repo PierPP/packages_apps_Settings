IceColdSandwich/android_packages_apps_Settings

Storage: Allow user to choose internal storage
Limited to devices that allow it (by presence of the ro.vold.switchablepair
property).

Ported from CM7's CMParts change I0f307858a0d443a295b30bffd8d66e89c425a13c

Change-Id: I73439e167d0e2a08e2e922d5c0930cdeebe64940

src/com/android/settings/deviceinfo/StorageSettings.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.ContentQueryMap;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+/**
+ * Generic storage settings.
+ */
+public class StorageSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "StorageSettings";
+
+    private static final String SWITCH_STORAGE_PREF = "pref_switch_storage";
+
+    private CheckBoxPreference mSwitchStoragePref;
+
+    private PreferenceScreen createPreferenceHierarchy() {
+        PreferenceScreen root = getPreferenceScreen();
+        if (root != null) {
+            root.removeAll();
+        }
+        addPreferencesFromResource(R.xml.storage_settings);
+        root = getPreferenceScreen();
+
+        mSwitchStoragePref = (CheckBoxPreference) root.findPreference(SWITCH_STORAGE_PREF);
+        mSwitchStoragePref.setChecked((SystemProperties.getInt("persist.sys.vold.switchexternal", 0) == 1));
+
+        if (SystemProperties.get("ro.vold.switchablepair","").equals("")) {
+            mSwitchStoragePref.setSummary(R.string.pref_storage_switch_unavailable);
+            mSwitchStoragePref.setEnabled(false);
+        }
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Make sure we reload the preference hierarchy since some of these settings
+        // depend on others...
+        createPreferenceHierarchy();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        // Don't allow any changes to take effect as the USB host will be disconnected, killing
+        // the monkeys
+        if (Utils.isMonkeyRunning()) {
+            return true;
+        }
+        if (preference == mSwitchStoragePref) {
+            SystemProperties.set("persist.sys.vold.switchexternal",
+               mSwitchStoragePref.isChecked() ? "1" : "0");
+        }
+        return true;
+    }
+}

PierPP/packages_apps_Settings

src/com/android/settings/deviceinfo/StorageSettings.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.ContentQueryMap;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+/**
+ * Generic storage settings.
+ */
+public class StorageSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "StorageSettings";
+
+    private static final String SWITCH_STORAGE_PREF = "pref_switch_storage";
+
+    private CheckBoxPreference mSwitchStoragePref;
+
+    private PreferenceScreen createPreferenceHierarchy() {
+        PreferenceScreen root = getPreferenceScreen();
+        if (root != null) {
+            root.removeAll();
+        }
+        addPreferencesFromResource(R.xml.storage_settings);
+        root = getPreferenceScreen();
+
+        mSwitchStoragePref = (CheckBoxPreference) root.findPreference(SWITCH_STORAGE_PREF);
+        mSwitchStoragePref.setChecked((SystemProperties.getInt("persist.sys.vold.switchexternal", 0) == 1));
+
+        if (SystemProperties.get("ro.vold.switchablepair","").equals("")) {
+            mSwitchStoragePref.setSummary(R.string.storage_switch_unavailable);
+            mSwitchStoragePref.setEnabled(false);
+        }
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Make sure we reload the preference hierarchy since some of these settings
+        // depend on others...
+        createPreferenceHierarchy();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        // Don't allow any changes to take effect as the USB host will be disconnected, killing
+        // the monkeys
+        if (Utils.isMonkeyRunning()) {
+            return true;
+        }
+        if (preference == mSwitchStoragePref) {
+            SystemProperties.set("persist.sys.vold.switchexternal",
+               mSwitchStoragePref.isChecked() ? "1" : "0");
+        }
+        return true;
+    }
+}